8173bef6c27f0bea1a5e4f4059ce164bf6e013bf
Albertojuanse <alberto.sebastian@uam.es>
2019-04-09 17:40:21 +0200

    Initial commit

---
+++ sistemaexperto/motordeinferencia.py
@@ -0,0 +1,3 @@
+""" Un sistema experto resuelve un problemas lógico-matemáticos mediante una base de reglas del tipo SI (condicion) THEN
+ (resultado) que representa el conocimiento del experto, una base de hechos, derivados o no de la aplicación de esas
+ reglas, y un motor de inferencia deductivo o inductivo capaz de evaluar ambos conjuntos y obtener una solución."""
